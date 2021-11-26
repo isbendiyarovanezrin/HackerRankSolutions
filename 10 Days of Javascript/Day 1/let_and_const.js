7f26ec688164d97f83a9d8bd6337bc883aa2d459
Nəzrin <isbndyrva@gmail.com>
2021-11-26 10:29:59 +0400

    Add 10 days of JavaScript solutions

---
+++ 10 Days of Javascript/Day 1/let_and_const.js
@@ -0,0 +1,15 @@
+// Language: JavaScript (Node.js)
+
+function main() {
+  // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+  let r = readLine();
+  const p = Math.PI;
+  const area = p * r ** 2;
+  const perimeter = 2 * p * r;
+
+  // Print the area of the circle:
+  console.log(area);
+
+  // Print the perimeter of the circle:
+  console.log(perimeter);
+}
